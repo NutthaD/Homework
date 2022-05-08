7e58035ae8167b76e8e52546770a63b91b821903
NutthaD <nuttha.ditthaporn@gmail.com>
2022-05-08 22:10:36 +0700

    Enter

---
+++ Part3/hw1-resent.js
@@ -0,0 +1,82 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+let employee_data = []
+
+app.get('/get_data',(req,res) => {
+    res.send(employee_data)
+})
+
+app.post('/post_data',(req,res) => {
+    if(!req.body.Fname ||
+        !req.body.Lname ||
+        !req.body.ID ||
+        !req.body.Position ||
+        !req.body.Tel ||
+        !req.body.Email){
+            return res.status(400).send("Error : blank data");
+        }
+
+    for (let i = 0; i<employee_data.length; i++){
+        if(employee_data[i].Employee_ID == req.body.ID ||
+            employee_data[i].Telephone == req.body.Tel ||
+            employee_data[i].Email == req.body.Email){
+                return res.status(400).send("Error : already have data")
+        }
+    }
+    //console.log(req.body)
+
+    let data = {
+        Firstname: req.body.Fname,
+        Lastname: req.body.Lname,
+        Employee_ID: req.body.ID,
+        Position: req.body.Position,
+        Telephone: req.body.Tel,
+        Email: req.body.Email
+    };
+
+    employee_data.push(data);
+    res.send("Add data success");
+})
+
+app.put('/update_data',(req,res) => {
+    if(!req.body.ID ||
+        !req.body.Position ||
+        !req.body.Tel ||
+        !req.body.Email){
+            return res.status(400).send("Error : blank data");
+        }
+    
+        for(let i = 0; i< employee_data.length; i++){
+            if(employee_data[i].Employee_ID == req.body.ID){
+                employee_data[i].Position = req.body.Position
+                employee_data[i].Tel = req.body.Telephone
+                employee_data[i].Email = req.body.Email
+                return res.send("Update success")
+            }
+        }
+
+        return res.status(400).send("Error : not found");
+})
+
+app.delete('/delete_data',(req,res) => {
+    if(!req.body.ID){
+        return res.status(400).send("Error : blank data");
+    }
+
+    for(let i = 0; i< employee_data.length; i++){
+        if(employee_data[i].Employee_ID == req.body.ID){
+            //remove data from array ลบที่ index i ไป 1 ตัว ,หลังจากนี้คือ item ข้างใน
+            employee_data.slice(i, 1)
+            return res.send("Delete success")
+        }
+    }
+
+    return res.status(400).send("Error : not found");
+})
+
+app.listen(3000 , () => {
+    console.log('Listening on port: 3000');
+});
